d27745206733fb46f0365645fc999e9871d79da2
dinakar_i <115007967+dinakar-i@users.noreply.github.com>
2025-09-08 23:29:26 +0530

    docker files moved to root

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7585098b33527e3b2bd2c46b54d878b0e047978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1bc2807aaf84440010a1457ac29b1b53fcdd92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
