d684675ff48a1942f0ee86167f1e4dca4a82d59f
dinakar_i <115007967+dinakar-i@users.noreply.github.com>
2025-10-18 02:18:48 +0530

    langing page design changed

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './Shared/navbar/navbar.component';
 import { SidebarComponent } from './Shared/sidebar/sidebar.component';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, CommonModule],
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isLandingPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.isLandingPage = this.router.url === '/';
+    });
+  }
+}
