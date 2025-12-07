66b4b7d8a698b0a721638319a3937d1947509dae
dinakar_i <115007967+dinakar-i@users.noreply.github.com>
2025-12-07 06:21:26 +0530

    build changes

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c3864c82b44d19dd9968de34e01a785c70b630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9154fc83279b4666f4d6486b2a76b9b2bfcb1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
